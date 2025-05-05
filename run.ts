0ff7e9e8cb11e120d2d901fcd2b59bcebb19b8cc
Sagarika Dasgupta <saga.dasgupta@shopify.com>
2025-05-05 09:37:20 -0500

    adding backwards compatibility for Javy globals

--- run.ts
+++ run.ts
@@ -2,27 +2,37 @@ export type UserFunction<Input extends {}, Output extends {}> = (
   input: Input
 ) => Output;
 
+interface Javy {
+  JSON: {
+    fromStdin(): any;
+    toStdout(val: any);
+  }
+}
+
 interface ShopifyFunction {
   readInput(): any;
   writeOutput(val: any);
 }
 
 declare global {
+  const Javy: Javy;
   const ShopifyFunction: ShopifyFunction;
 }
 
 export default function <I extends {}, O extends {}>(
   userfunction: UserFunction<I, O>
 ) {
-  try {
-    ShopifyFunction;
-  } catch (e) {
+  if (Javy.JSON) {
+    const input_obj = Javy.JSON.fromStdin();
+    const output_obj = userfunction(input_obj);
+    Javy.JSON.toStdout(output_obj);
+  } else if (ShopifyFunction) {
+    const input_obj = ShopifyFunction.readInput();
+    const output_obj = userfunction(input_obj);
+    ShopifyFunction.writeOutput(output_obj);
+  } else {
     throw new Error(
       "ShopifyFunction is not defined. Please rebuild your function using the latest version of Shopify CLI."
     );
   }
-
-  const input_obj = ShopifyFunction.readInput();
-  const output_obj = userfunction(input_obj);
-  ShopifyFunction.writeOutput(output_obj);
 }
